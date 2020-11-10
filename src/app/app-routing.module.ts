953afa7455b4b76b938e51e8371b5ca576ba507a
Rodrigo Sturm <rodrigoks@gmail.com>
2020-11-10 15:14:18 -0300

    Configuracao de rotas de navegacao do componente principal.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+export const ROUTES: Routes = [
+
+];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(ROUTES) ],
+    exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
