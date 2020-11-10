67aec81ded985d3e420249edf69ef07ce60f3a20
Rodrigo Sturm <rodrigoks@gmail.com>
2020-11-10 19:28:40 -0300

    Criando interface html listar tarefas.

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-tarefa',
   templateUrl: './listar-tarefa.component.html',
-  styleUrls: ['./listar-tarefa.component.css']
+  styleUrls: ['./listar-tarefa.component.css'],
+  preserveWhitespaces: true
 })
 export class ListarTarefaComponent implements OnInit {
 
